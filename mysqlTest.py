adb663b8065866c464908c6aea774421e95d7ae9
Danna E Wroblewski <danna.basschic@gmail.com>
2018-05-10 23:10:13 -0700

    Added MySQL model file, mysql connection file, and working example of database connection on local computer

---
+++ mysqlTest.py
@@ -0,0 +1,15 @@
+from flask import Flask
+# import the function connectToMySQL from the file mysqlconnection.py
+from mysqlconnection import connectToMySQL
+app = Flask(__name__)
+# invoke the connectToMySQL function and pass it the name of the database we're using
+# connectToMySQL returns an instance of MySQLConnection, which we will store in the variable 'mysql'
+mysql = connectToMySQL('thegreatwall')
+# now, we may invoke the query_db method
+@app.route('/')
+def index():
+    print("all the users", mysql.query_db("SELECT * FROM users;"))
+    return "How am I going to put this on my laptop?"
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_bcrypt import Bcrypt
+from mysqlconnection import MySQLConnector
+import md5
+
+mysql = MySQLConnector(app,'the_wall')
+
 app = Flask(__name__)
 bcrypt = Bcrypt(app) # creating an object called bcrypt
 app.secret_key = '1fish2fishredfishbluefish' #setting secret key
@@ -7,13 +12,12 @@ app.secret_key = '1fish2fishredfishbluefish' #setting secret key
 def index():
     return render_template("index.html")
 
-@app.route('/createUser', methods=['POST'])
-def create():
-    pw-hash = bcrypt.generate_password_hash(request.form['password'])
-    print(pw_hash)
 
-    query = "INSERT INTO users (username, password) VALUE (%(username)s, %(password_hash)s);"
-    data = { "username" : request.from['username'],
+@app.route('/login', methods=['POST'])
+def login():
+    query = "SELECT * FROM users where email = :email"
+
+    data = { "username" : request.form['username'],
              "password_hash" : pw_hash }
     result = mysql.query_db(query, data)
     if result:
@@ -26,11 +30,30 @@ def create():
     flash("You could not be loggin in")
     return redirect('/')
 
+# @app.route('/createUser', methods=['POST'])
+# def createUser():
+#     pw_hash = bcrypt.generate_password_hash(request.form['password'])
+#     print(pw_hash)
+#
+#     query = "INSERT INTO users (username, password) VALUE (%(username)s, %(password_hash)s);"
+#     data = { "username" : request.form['username'],
+#              "password_hash" : pw_hash }
+#     result = mysql.query_db(query, data)
+#     if result:
+#         if bcrypt.check_password_hash(result[0]['password'], request.form['password']):
+#             session['userid'] = result[0]['id']
+#             return redirect('/success')
+#
+#     return redirect('/')
+#
+#     flash("You could not be loggin in")
+#     return redirect('/')
+
 @app.route('/users', methods=['POST'])
 def create():
     print(request.form)
     print('Name', request.form[name])
     print('')
 
-if __name__=="__main__"
+if __name__ =="__main__":
     app.run(debug=True)
